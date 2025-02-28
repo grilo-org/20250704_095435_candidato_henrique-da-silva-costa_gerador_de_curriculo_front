2ff40c415618c2e2c9f4762fc0b8be241c14e39a
Henrique <81424965+henrique-da-silva-costa@users.noreply.github.com>
2025-02-28 08:40:47 -0300

    ajuste de titulo

--- src/pages/CurriculoExperiencias.jsx
+++ src/pages/CurriculoExperiencias.jsx
@@ -83,7 +83,7 @@ const CurriculoExperiencias = () => {
         <>
             <Button color="transparent" onClick={() => nav("/curriculos")}><FaArrowLeft size={40} /></Button>
             <Container>
-                <h1>Experiências</h1>
+                <h1>Adicionar experiências</h1>
                 <div className="text-end d-flex gap-2 justify-content-end">
                     <Cadastrar tamanhoBotao={"sm"} tipoFormulario={"experiencias"} inputs={inputs} url={"cadastrar/experiencia"} pegarDadosCarregar={pegarDados} />
                     <Button className={styles.fonteBotao12} size="sm" color="primary" onClick={() => pegarCurriculo(curriculoId)}>VER CURRICULO</Button>
